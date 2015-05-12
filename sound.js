d653b55420a0f281e2d40272e540cbfa552eb1b6
Bradley Jewell <brad811@gmail.com>
2015-05-12 16:13:20 -0500

    rearrange javascript a little bit

--- sound.js
+++ sound.js
@@ -1,37 +1,3 @@
-var hsv2rgb = function(h, s, v) {
-	var rgb, i, data = [];
-	if (s === 0) {
-		rgb = [v,v,v];
-	} else {
-		h = h / 60;
-		i = Math.floor(h);
-		data = [v*(1-s), v*(1-s*(h-i)), v*(1-s*(1-(h-i)))];
-		switch(i) {
-			case 0:
-				rgb = [v, data[2], data[0]];
-				break;
-			case 1:
-				rgb = [data[1], v, data[0]];
-				break;
-			case 2:
-				rgb = [data[0], v, data[2]];
-				break;
-			case 3:
-				rgb = [data[0], data[1], v];
-				break;
-			case 4:
-				rgb = [data[2], data[0], v];
-				break;
-			default:
-				rgb = [v, data[0], data[1]];
-				break;
-		}
-	}
-	return '#' + rgb.map(function(x){
-		return ("0" + Math.round(x*255).toString(16)).slice(-2);
-	}).join('');
-};
-
 var freqByteData = 0;
 var analyzer = null;
 var input = null;
@@ -118,6 +84,40 @@ MediaStreamTrack.getSources(function(sourceInfos) {
 
 	navigator.getUserMedia(constraints, successCallback, errorCallback);
 });
+
+var hsv2rgb = function(h, s, v) {
+	var rgb, i, data = [];
+	if (s === 0) {
+		rgb = [v,v,v];
+	} else {
+		h = h / 60;
+		i = Math.floor(h);
+		data = [v*(1-s), v*(1-s*(h-i)), v*(1-s*(1-(h-i)))];
+		switch(i) {
+			case 0:
+				rgb = [v, data[2], data[0]];
+				break;
+			case 1:
+				rgb = [data[1], v, data[0]];
+				break;
+			case 2:
+				rgb = [data[0], v, data[2]];
+				break;
+			case 3:
+				rgb = [data[0], data[1], v];
+				break;
+			case 4:
+				rgb = [data[2], data[0], v];
+				break;
+			default:
+				rgb = [v, data[0], data[1]];
+				break;
+		}
+	}
+	return '#' + rgb.map(function(x){
+		return ("0" + Math.round(x*255).toString(16)).slice(-2);
+	}).join('');
+};
 
 var resizeCanvas = function() {
 	var w = window,
